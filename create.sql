f47625e749df35bc82cd5302c96ffb09385df150
Alexandre Queiroz <alexandrequeiroz.dev@gmail.com>
2024-05-31 18:27:46 -0300

    feat: change sql time

--- create.sql
+++ create.sql
@@ -18,7 +18,7 @@ CREATE TABLE patterns.payment (
 );
 
 INSERT INTO patterns.contract (id_contract, description, amount, periods, date)
-VALUES ('4224a279-c162-4283-86f5-1095f559b08c', 'Prestação de serviços escolares', 6000, 12, '2024-01-01T10:00:00');
+VALUES ('4224a279-c162-4283-86f5-1095f559b08c', 'Prestação de serviços escolares', 6000, 12, '2022-01-01T10:00:00');
 
 INSERT INTO patterns.payment (id_payment, id_contract, amount, date)
-VALUES ('c931d9db-c8d8-44d4-8861-b3d6b734c64e', '4224a279-c162-4283-86f5-1095f559b08c', 6000, '2024-01-05T10:00:00');
+VALUES ('c931d9db-c8d8-44d4-8861-b3d6b734c64e', '4224a279-c162-4283-86f5-1095f559b08c', 6000, '2022-01-05T10:00:00');
